6041ca13d618afe55f7bd60c856205221cff1d06
Csiszár Tamás <Csiszr Tams@DESKTOP-0NUSROK>
2022-01-05 11:33:21 +0100

    Add project files.

---
+++ WFA_pendulum/FrmMain.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace WFA_pendulum
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.lbArtist = new System.Windows.Forms.Label();
+            this.lbSearch = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbUrl = new System.Windows.Forms.Label();
+            this.llbLink = new System.Windows.Forms.LinkLabel();
+            this.lbAlbum = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 45);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(157, 45);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // lbArtist
+            // 
+            this.lbArtist.AutoSize = true;
+            this.lbArtist.Location = new System.Drawing.Point(45, 20);
+            this.lbArtist.Name = "lbArtist";
+            this.lbArtist.Size = new System.Drawing.Size(30, 13);
+            this.lbArtist.TabIndex = 2;
+            this.lbArtist.Text = "Artist";
+            // 
+            // lbSearch
+            // 
+            this.lbSearch.AutoSize = true;
+            this.lbSearch.Location = new System.Drawing.Point(16, 93);
+            this.lbSearch.Name = "lbSearch";
+            this.lbSearch.Size = new System.Drawing.Size(105, 13);
+            this.lbSearch.TabIndex = 4;
+            this.lbSearch.Text = "Search in track\'s title";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(19, 176);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(398, 241);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(19, 123);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(398, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(434, 176);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(109, 104);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lbUrl
+            // 
+            this.lbUrl.AutoSize = true;
+            this.lbUrl.Location = new System.Drawing.Point(449, 319);
+            this.lbUrl.Name = "lbUrl";
+            this.lbUrl.Size = new System.Drawing.Size(63, 13);
+            this.lbUrl.TabIndex = 8;
+            this.lbUrl.Text = "URL(if any):";
+            // 
+            // llbLink
+            // 
+            this.llbLink.AutoSize = true;
+            this.llbLink.Location = new System.Drawing.Point(518, 319);
+            this.llbLink.Name = "llbLink";
+            this.llbLink.Size = new System.Drawing.Size(0, 13);
+            this.llbLink.TabIndex = 9;
+            // 
+            // lbAlbum
+            // 
+            this.lbAlbum.AutoSize = true;
+            this.lbAlbum.Location = new System.Drawing.Point(197, 20);
+            this.lbAlbum.Name = "lbAlbum";
+            this.lbAlbum.Size = new System.Drawing.Size(36, 13);
+            this.lbAlbum.TabIndex = 10;
+            this.lbAlbum.Text = "Album";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbAlbum);
+            this.Controls.Add(this.llbLink);
+            this.Controls.Add(this.lbUrl);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lbSearch);
+            this.Controls.Add(this.lbArtist);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "FrmMain";
+            this.Text = "Discograpy Tracker";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label lbArtist;
+        private System.Windows.Forms.Label lbSearch;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbUrl;
+        private System.Windows.Forms.LinkLabel llbLink;
+        private System.Windows.Forms.Label lbAlbum;
+    }
+}
+
